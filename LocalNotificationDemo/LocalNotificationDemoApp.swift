811d72fa4bf1169377935c18e9dabd30edcf9162
chanoktrue <chanoktrue@me.com>
2024-07-18 21:31:32 +0700

    Initial Commit

---
+++ LocalNotificationDemo/LocalNotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationDemoApp.swift
+//  LocalNotificationDemo
+//
+//  Created by Thongchai Subsaidee on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
